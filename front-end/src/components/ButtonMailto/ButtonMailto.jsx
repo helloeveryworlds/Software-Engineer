265ce381de4f73193dde748d8fdab045b52a2755
ZhengZhang <zhzheng@bu.edu>
2023-02-19 16:44:55 -0500

    Button Mailto component

---
+++ front-end/src/components/ButtonMailto/ButtonMailto.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./ButtonMailto.css";
+
+class ButtonMailto extends React.Component {
+  render() {
+    return (
+      <Link
+        to="#"
+        onClick={(e) => {
+          window.location.href = this.props.mailto;
+          e.preventDefault();
+        }}
+        className="footer-email"
+      >
+        {this.props.label}
+      </Link>
+    );
+  }
+}
+
+export default ButtonMailto;
