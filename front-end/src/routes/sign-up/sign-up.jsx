c95cdf66af0b1fd289211ce8d97d5dd7a479a770
QiweiL1 <Qiwe1Li@126.com>
2023-03-18 07:40:36 -0400

    Sign up function, not test yet

--- front-end/src/routes/sign-up/sign-up.jsx
+++ front-end/src/routes/sign-up/sign-up.jsx
@@ -1,58 +1,57 @@
 import React from "react";
 import { Fragment } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./sign-up.css";
 
 class SignUp extends React.Component {
-
   constructor(props) {
     super(props);
-    this.handleChangeName = this.handleChangeName.bind(this)
-    this.handleChangeEmail = this.handleChangeEmail.bind(this)
-    this.handleChangePwd = this.handleChangePwd.bind(this)
-    this.handleChangeAddress = this.handleChangeAddress.bind(this)
-    this.handleChangeZipcode = this.handleChangeZipcode.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePwd = this.handleChangePwd.bind(this);
+    this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    this.handleChangeZipcode = this.handleChangeZipcode.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       name: "",
       email: "",
       password: "",
       address: "",
-      zipcode: ""
-    }
+      zipcode: "",
+    };
   }
 
   handleChangeName(e) {
     this.setState({
-      name: e.target.value
-    })
+      name: e.target.value,
+    });
   }
 
   handleChangeEmail(e) {
     this.setState({
-      email: e.target.value
-    })
+      email: e.target.value,
+    });
   }
 
   handleChangePwd(e) {
     this.setState({
-      password: e.target.value
-    })
+      password: e.target.value,
+    });
   }
 
   handleChangeAddress(e) {
     this.setState({
-      address: e.target.value
-    })
+      address: e.target.value,
+    });
   }
 
   handleChangeZipcode(e) {
     this.setState({
-      zipcode: e.target.value
-    })
+      zipcode: e.target.value,
+    });
   }
 
-  handleSubmit(e) {
+  /*   handleSubmit(e) {
     e.preventDefault()
     axios.post(
       "http://localhost:8080/signup",
@@ -74,6 +73,22 @@ class SignUp extends React.Component {
           alert(error.message)
       }
     });
+  } */
+
+  handleSubmit(e) {
+    const signupUrl = `/signup?username=${this.state.name}&email=${this.state.email}&password=${this.state.password}&address=${this.state.address}&zipcode=${this.state.zipcode}`;
+
+    fetch(signupUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }).then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        throw Error("Fail to sign up");
+      }
+    });
   }
 
   render() {
@@ -82,11 +97,41 @@ class SignUp extends React.Component {
         <div className="signupbox">
           <h1>Create Account</h1>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.name} onChange={this.handleChangeName} name="" placeholder="Name" />
-            <input type="text" value={this.state.email} onChange={this.handleChangeEmail} name="" placeholder="Email" />
-            <input type="password" value={this.state.password} onChange={this.handleChangePwd} name="" placeholder="Password" />
-            <input type="text" name="" value={this.state.address} onChange={this.handleChangeAddress} placeholder="Address" />
-            <input type="text" name="" value={this.state.zipcode} onChange={this.handleChangeZipcode} placeholder="Zip code" />
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChangeName}
+              name=""
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.handleChangeEmail}
+              name=""
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChangePwd}
+              name=""
+              placeholder="Password"
+            />
+            <input
+              type="text"
+              name=""
+              value={this.state.address}
+              onChange={this.handleChangeAddress}
+              placeholder="Address"
+            />
+            <input
+              type="text"
+              name=""
+              value={this.state.zipcode}
+              onChange={this.handleChangeZipcode}
+              placeholder="Zip code"
+            />
             <input type="submit" name="" value="Submit" />
             <p className="signinlink">
               Have an account?
