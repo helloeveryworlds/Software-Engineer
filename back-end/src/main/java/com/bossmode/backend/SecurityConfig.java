ca04f56c786c5f251ba57d2aa72b3d2f62bab77b
helloeveryworlds <1121636000@qq.com>
2023-03-29 16:12:01 -0400

    update crsf status

--- back-end/src/main/java/com/bossmode/backend/SecurityConfig.java
+++ back-end/src/main/java/com/bossmode/backend/SecurityConfig.java
@@ -18,7 +18,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .formLogin()
                 .failureForwardUrl("/login?error=true");
         http
