595e47d1bd0797a0cf2de02c5ff83bd4dbda41da
helloeveryworlds <1121636000@qq.com>
2023-04-23 16:15:59 -0400

    add restrict on delete

--- back-end/src/main/java/com/bossmode/backend/SecurityConfig.java
+++ back-end/src/main/java/com/bossmode/backend/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/signup").permitAll() // Add this line to allow access to /signup
-                .antMatchers("/order/*", "/cart", "/checkout").hasAuthority("ROLE_USER")
+                .antMatchers("/order/*", "/cart", "/checkout", "/delete/*").hasAuthority("ROLE_USER")
                 .anyRequest().permitAll();
     }
 
